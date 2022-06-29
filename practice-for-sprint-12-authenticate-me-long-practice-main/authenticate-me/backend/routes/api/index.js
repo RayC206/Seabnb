29e0a6fde66d0c27c76b74f4ea08b313d0fb4bd8
RayC206 <rcv.henry@gmail.com>
2022-06-29 14:20:33 -0700

    first commit

---
+++ practice-for-sprint-12-authenticate-me-long-practice-main/authenticate-me/backend/routes/api/index.js
@@ -0,0 +1,52 @@
+const router = require('express').Router();
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+// GET /api/set-token-cookie
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user });
+});
+
+// GET /api/restore-user
+
+router.use(restoreUser);
+
+router.get(
+  '/restore-user',
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+
+const { requireAuth } = require('../../utils/auth.js');
+router.get(
+  '/require-auth',
+  requireAuth,
+  (req, res) => {
+    return res.json(req.user);
+  }
+);
+
+module.exports = router;
