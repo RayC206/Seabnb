032ee7d53fa3a5cc1ab266c4f4581574590d6d21
RayC206 <rcv.henry@gmail.com>
2022-07-05 11:00:45 -0700

    added check for email/username for log in credential

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -3,17 +3,24 @@ const express = require('express');
 const { setTokenCookie, restoreUser } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
-const { check } = require('express-validator');
+const { check, oneOf } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 
 const router = express.Router();
 
 
 const validateLogin = [
-  check('credential')
-    .exists({ checkFalsy: true })
-    .notEmpty()
-    .withMessage('Please provide a valid email or username.'),
+  oneOf([ // check if either username or email has been set
+    check('username')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+
+    check('email')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+  ]),
   check('password')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a password.'),
@@ -25,7 +32,13 @@ router.post(
   '/',
   validateLogin,
   async (req, res, next) => {
-    const { credential, password } = req.body;
+    const { username, email, password } = req.body;
+
+    if (username) {
+      credential = username;
+    } else if (email) {
+      credential = email;
+    }
 
     const user = await User.login({ credential, password });
 
