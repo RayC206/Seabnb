39cf965646f2acd66d1c45b7240e6d62666ecba3
RayC206 <rcv.henry@gmail.com>
2022-07-25 12:26:08 -0700

    working on getting image array displayed on spotDetail

--- frontend/src/components/SpotDetail/index.js
+++ frontend/src/components/SpotDetail/index.js
@@ -1,35 +1,51 @@
-import React, {  useEffect } from "react";
-import {  useParams } from "react-router-dom";
+import React, { useEffect } from "react";
+import { useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
-import { findASpot } from "../../store/spots"
-
-
+import { findASpot } from "../../store/spots";
 
 const SpotsDetail = () => {
+  let { spotId } = useParams();
+  spotId = Number(spotId);
   const dispatch = useDispatch();
-  let { spotId } = useParams()
-  spotId = Number(spotId)
-  const spot= useSelector((state) => Object.values(state.spots[spotId]));
-  console.log(spot)
+  const spot = useSelector((state) => state.spots);
+  console.log(spot);
+  // const images = spot.images;
+  // console.log(images);
+  
+  // spot.images.forEach((image) => {
+  //   console.log(image);
+  // });
+
+  // const spot= useSelector((state) => Object.values(state.spots.spotId));
+  // console.log(spot)
 
   useEffect(() => {
-    dispatch(findASpot());
-
-}, [dispatch])
-
-
+    dispatch(findASpot(spotId));
+  }, [dispatch]);
 
   return (
-    <div>{spot.map(spot => (
+    <div>
+      {/* {spot.map((spot) => ( */}
       <div key={spot.id}>
-          <h3>{spot.name}</h3>
-          <h4>{spot.city}, {spot.state}</h4>
-          <img src={spot.previewImage} alt= {spot.name}></img>
-          <p>{spot.description}</p>
-          <p> Price: ${spot.price}</p>
+        <h3>{spot.name}</h3>
+        <h4>
+          {spot.city}, {spot.state}
+        </h4>
+        <div>{spot.images}</div>
+
+        <div>
+          {/* {spot.images.map((image) => (
+            <div>{image}</div>
+            // <img src={image} alt={spot.name}></img> */}
+          {/* ))} */}
+        </div>
+        <p>{spot.description}</p>
+        <p> Price: ${spot.price}</p>
       </div>
-  ))}</div>
-  )
-  }
+      {/* ))} */}
+    </div>
+
+  );
+};
 
 export default SpotsDetail;
