5124c93e11495cea60a2bbabfaa4073ab806a7e7
RayC206 <rcv.henry@gmail.com>
2022-07-25 09:56:05 -0700

    displaying all spots with images, need to fix image size

--- frontend/src/components/SpotDetail/index.js
+++ frontend/src/components/SpotDetail/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllSpots, findASpot } from "../../store/spots"
-// import "./SpotDetail.css"
+
 
 
 const SpotsDetail = () => {
--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -1,30 +1,33 @@
 import React, { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
 import { getAllSpots, findASpot } from "../../store/spots"
-// import "./SpotDetail.css"
+
+
 
 
 const SpotsPage = () => {
   const dispatch = useDispatch();
-  const spots = useSelector((state) => Object.values(state.spots));
-  console.log(spots)
+  const spotsList = useSelector((state) => Object.values(state.spots));
+  console.log(spotsList)
 
   useEffect(() => {
     dispatch(getAllSpots());
-    window.scrollTo(0, 0);
+
 }, [dispatch])
 
 
 
-  return (
-    <>
-      <h1>Test</h1>
-      <div>
-      </div>
-
-    </>
-  )
+return (
+  <div>{spotsList.map(spot => (
+    <div key={spot.id}>
+        <h3>{spot.name}</h3>
+        <h4>{spot.city}, {spot.state}</h4>
+        <img src={spot.previewImage}></img>
+        <p>{spot.description}</p>
+        <p> Price: ${spot.price}</p>
+    </div>
+))}</div>
+)
 }
 
 export default SpotsPage;
