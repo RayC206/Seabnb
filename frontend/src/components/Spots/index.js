aa316f270874491e8e76bdb6da91a260c37cc586
RayC206 <rcv.henry@gmail.com>
2022-07-30 10:18:26 -0700

    debugging

--- frontend/src/components/Spots/index.js
+++ frontend/src/components/Spots/index.js
@@ -21,25 +21,27 @@ const SpotsPage = () => {
         if (spot) {
           return (
             <div key={index}>
-            <NavLink to={`/spots/${spot.id}`}>
-              <div className="eachSpot" key={spot.id}>
-                <img
-                  className="spotImg"
-                  src={spot.previewImage}
-                  alt={spot.name}
-                ></img>
-                <h3 className="spotName">{spot.name}</h3>
-                <h4 className="spotLocation">
-                  {spot.city}, {spot.state}
-                </h4>
-                <p className="spotAddress">{spot.address}</p>
-                <p className="spotDetails">{spot.description}</p>
-                <p className="spotPrice"> ${spot.price} night</p>
-                {spot.avgStarRating && (
-                  <p className="spotAvgStarRating">Average Rating: {spot.avgStarRating.toString()} / 5</p>
-                )}
-              </div>
-            </NavLink>
+              <NavLink to={`/spots/${spot.id}`}>
+                <div className="eachSpot" key={spot.id}>
+                  <img
+                    className="spotImg"
+                    src={spot.previewImage}
+                    alt={spot.name}
+                  ></img>
+                  <h3 className="spotName">{spot.name}</h3>
+                  <h4 className="spotLocation">
+                    {spot.city}, {spot.state}
+                  </h4>
+                  <p className="spotAddress">{spot.address}</p>
+                  <p className="spotDetails">{spot.description}</p>
+                  <p className="spotPrice"> ${spot.price} night</p>
+                  {spot.avgStarRating && (
+                    <p className="spotAvgStarRating">
+                      Average Rating: {spot.avgStarRating} / 5
+                    </p>
+                  )}
+                </div>
+              </NavLink>
             </div>
           );
         }
