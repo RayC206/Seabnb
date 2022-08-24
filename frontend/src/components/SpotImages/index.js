6f3020c751759280ad70a04d0d102021aefffcd8
RayC206 <rcv.henry@gmail.com>
2022-08-24 14:14:01 -0700

    Image page for authorized and unauthorized user complete

--- frontend/src/components/SpotImages/index.js
+++ frontend/src/components/SpotImages/index.js
@@ -5,8 +5,6 @@ import { deleteSpotImage, findASpot } from "../../store/spots";
 import { FaTrashAlt } from "react-icons/fa";
 import "../CSS/SpotImages.css";
 
-
-
 const SpotImages = () => {
   let { spotId } = useParams();
   spotId = Number(spotId);
@@ -17,6 +15,7 @@ const SpotImages = () => {
   const [findASpotStatus, setFindASpotStatus] = useState(200);
   const [isLoaded, setIsLoaded] = useState(false);
   const [imageFormModalIsOpen, setImageFormModalIsOpen] = useState(false);
+  const viewingAsOwner = sessionUser.id === spot.ownerId;
 
   // console.log(spot);
 
@@ -42,52 +41,79 @@ const SpotImages = () => {
   };
 
   const spotsPage = () => {
-    let route = `/spots/${spot.id}`
-    history.push(route)
-  }
+    let route = `/spots/${spot.id}`;
+    history.push(route);
+  };
 
   const imageFormPage = () => {
-    let route = `/spots/${spot.id}/images/add-image`
-    history.push(route)
-  }
-
+    let route = `/spots/${spot.id}/images/add-image`;
+    history.push(route);
+  };
 
   if (isLoaded) {
     if (findASpotStatus === 200) {
-      return (
-        <div className="imagePageContainer">
-          <div className="imagePageButtons">
-            <button className="backButton" onClick={spotsPage}>
-              Go Back
-            </button>
-            <button className="backButton" onClick={imageFormPage}>
-              Add Image
-            </button>
+      if (viewingAsOwner) {
+        return (
+          <div className="imagePageContainer">
+            <div className="imagePageButtons">
+              <button className="backButton" onClick={spotsPage}>
+                Go Back
+              </button>
+              <button className="backButton" onClick={imageFormPage}>
+                Add Image
+              </button>
+            </div>
+            <div>
+              <h1></h1>
+              {spot.images.map((image) => {
+                return (
+                  <div className="imageGridContainer">
+                    <div className="imagePage-grid">
+                      <img
+                        className=""
+                        key={image.id}
+                        src={image.url}
+                        alt={spot.name}
+                      ></img>
+                      <FaTrashAlt
+                        className="deleteButton"
+                        onClick={(e) => handleDelete(e, image.id)}
+                      />
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
           </div>
-          <div>
-            <h1></h1>
+        );
+      } else {
+        return (
+        <div className="imagePageContainer">
+            <button className="backButton" onClick={history.goBack}>
+            Go Back
+          </button>
+          <div className="imagePage-grid2">
+            <img
+              className="imagePage-grid-col-2 imagePage-grid-row-2"
+              src={spot.previewImage}
+              alt={spot.name}
+            ></img>
             {spot.images.map((image) => {
               return (
-                <div className="imageGridContainer">
-                <div className="imagePage-grid">
+                <>
                   <img
                     className=""
                     key={image.id}
                     src={image.url}
                     alt={spot.name}
                   ></img>
-                  <FaTrashAlt
-                   className="deleteButton"
-                    onClick={(e) => handleDelete(e, image.id)}
-                    />
-               
-                </div>
-                </div>
+                </>
               );
             })}
           </div>
         </div>
-      );
+        )
+      }
     } else if (findASpotStatus === 404) {
       return (
         <div className="fourOhFour">
